5fd4833bfcb946414aa266797df37fe2299442ed
JJBZZRD <buzzard.jj@gmail.com>
2024-07-15 15:01:27 +0100

    initial WatsonModel structure

--- backend/models/watson_model.py
+++ backend/models/watson_model.py
@@ -1,3 +1,11 @@
 class WatsonModel:
     def __init__(self):
         self.model = None
+
+    def download(self, model_name):
+        pass
+    
+    def load(self, model_name):
+        pass
+    
+    
