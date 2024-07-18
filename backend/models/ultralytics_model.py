35ca2ba5bead41cb1ea0d4d48d959e67f375fea1
Aur01 <elfidaalima@gmail.com>
2024-07-18 22:13:10 +0100

    Update model index with detr and update training

--- backend/models/ultralytics_model.py
+++ backend/models/ultralytics_model.py
@@ -18,22 +18,27 @@ class UltralyticsModel(BaseModel):
         self.model_id = model_id
         self.model = None
         self.library_control = LibraryControl()
+        self.model_info = self.library_control.get_model_info_index(model_id)
+        self.model_type = self.model_info['requirements']['required_classes']['model']
     
     @staticmethod
-    def download(model_id: str, model_info: dict,):
+    def download(model_id: str, model_info: dict):
         try:
             model_dir = os.path.join('data', 'downloads', 'ultralytics')
             if not os.path.exists(model_dir):
                 os.makedirs(model_dir, exist_ok=True)
-            
-            model = YOLO(model_id) 
+
+            model_type = model_info['requirements']['required_classes']['model']
             model_file_name = f'{model_id}.pt' if not model_id.endswith('.pt') else model_id
             model_path = os.path.abspath(os.path.join(model_dir, model_file_name))
-            
-            print(f"Saving model to: {model_path}")
-            model.save(model_path)
+            if model_type == 'YOLO':
+                model = YOLO(model_id)
+                model.save(model_path)
+            else:
+                raise ValueError(f"Unsupported model type: {model_type}")
+
             print(f"Model {model_id} downloaded and saved to {model_path}")
-            
+
             root_model_path = os.path.join(ROOT_DIR, model_file_name)
             if os.path.exists(root_model_path):
                 os.remove(root_model_path)
@@ -43,7 +48,7 @@ class UltralyticsModel(BaseModel):
                 "base_model": model_id,
                 "dir": model_dir,
                 "is_customised": False,
-                "config":{}
+                "config": {}
             })
             return model_info
         except Exception as e:
@@ -54,37 +59,45 @@ class UltralyticsModel(BaseModel):
         try:
             # Retrieve model info from the library
             model_info = self.library_control.get_model_info_library(self.model_id)
-        
+
             if not model_info:
                 raise FileNotFoundError(f"Model info not found for {self.model_id}")
-        
+
             # Get the path of the model file
             model_file_name = f"{self.model_id}.pt"
             model_path = os.path.join(model_info['dir'], model_file_name)
-        
+
             if not os.path.exists(model_path):
                 raise FileNotFoundError(f"Model file not found: {model_path}")
+
+            if self.model_type == "YOLO":
+                self.model = YOLO(model_path)  # Load the YOLO model from the specified path
+            else:
+                raise ValueError(f"Unsupported model type: {self.model_type}")
         
-            self.model = YOLO(model_path)  # Load the YOLO model from the specified path
             logger.info(f"Model loaded from {model_path}")
-        
+
             # Set device based on user preference
             self.model.to(device)
-            
+        
         except Exception as e:
             logger.error(f"Error loading model: {str(e)}")
     
     def inference(self, request_payload: dict):
-        print("runned yolo inference function")
+        print("Running inference function")
+        
+        if self.model is None:
+            raise ValueError("Model is not loaded")
         if "image_path" in request_payload:
-            print("runned yolo inference")
+            print("Running image inference")
             return self.predict_image(request_payload["image_path"])
         elif "video_frame" in request_payload:
+            print("Running video inference")
             return self.predict_video(request_payload["video_frame"])
         else:
             return {"error": "Invalid request payload"}
     
-    # process request should be merged into inference
+    """# process request should be merged into inference
     def process_request(self, request_payload: dict):
         print("runned yolo inference function")
         if "image_path" in request_payload:
@@ -93,75 +106,40 @@ class UltralyticsModel(BaseModel):
         elif "video_frame" in request_payload:
             return self.predict_video(request_payload["video_frame"])
         else:
-            return {"error": "Invalid request payload"}
+            return {"error": "Invalid request payload"}"""
 
-    def predict_image(self, image_path: str):
+    def _predict_image(self, image_path: str):
         try:
-            if self.model is None:
-                raise ValueError("Model is not loaded")
-            
             results = self.model.predict(image_path)
-            predictions = []
-            class_names = self.model.names
-            
-            for result in results:
-                boxes = result.boxes
-                for box in boxes:
-                    cls = int(box.cls[0])
-                    conf = box.conf[0].item()
-                    coords = box.xyxy[0].tolist()
-                    predictions.append({
-                        "class": class_names[cls],
-                        "confidence": conf,
-                        "coordinates": coords
-                    })
-            print("runned yolo predict image")
-            return predictions
+            return self._process_results(results)
         except Exception as e:
             print(f"Error predicting image {image_path}: {str(e)}")
             return {"error": str(e)}
-        
-    def predict_video(self, frame: list):
+
+    def _predict_video(self, frame: list):
         try:
-            if self.model is None:
-                raise ValueError("Model is not loaded")
-        
-            print("Starting prediction on video frame")  
-        
             frame = np.array(frame, dtype=np.uint8)
             results = self.model.predict(frame)
-            predictions = []
-            class_names = self.model.names
-        
-            for result in results:
-                boxes = result.boxes
-                for box in boxes:
-                    cls = int(box.cls[0])
-                    conf = box.conf[0].item()
-                    coords = box.xyxy[0].tolist()
-                    predictions.append({
-                        "class": class_names[cls],
-                        "confidence": conf,
-                        "coordinates": coords
-                    })
-
-            print("Prediction on frame completed")  
-            return predictions
+            return self._process_results(results)
         except Exception as e:
             print(f"Error predicting video frame: {str(e)}")
             return {"error": str(e)}
 
+    def _process_results(self, results):
+        predictions = []
+        for result in results:
+            boxes = result.boxes
+            for box in boxes:
+                cls = int(box.cls[0])
+                conf = box.conf[0].item()
+                coords = box.xyxy[0].tolist()
+                predictions.append({
+                    "class": self.model.names[cls],
+                    "confidence": conf,
+                    "coordinates": coords
+                })
+        return predictions
 
-    """def train(self, data_path: str, epochs: int = 20):
-        try:
-            if self.model is None:
-                raise ValueError("Model is not loaded")
-            
-            self.model.train(data=data_path, epochs=epochs)
-            logger.info(f"Model trained on {data_path} for {epochs} epochs")
-        except Exception as e:
-            logger.error(f"Error training model on data {data_path}: {str(e)}")"""
-            
     def train(self, data_path: str, epochs: int = 10, batch_size: int = 16, learning_rate: float = 0.001):
         try:
             if self.model is None:
