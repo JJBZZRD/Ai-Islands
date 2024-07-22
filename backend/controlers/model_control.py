4a5fea1779f734305eac44f66f68f796a5366715
bennylao <bennylao830@gmail.com>
2024-07-22 22:20:34 +0100

    update index

--- backend/controlers/model_control.py
+++ backend/controlers/model_control.py
@@ -50,6 +50,10 @@ class ModelControl:
             if req == "terminate":
                 conn.send("Terminating")
                 break
+            # the first if block is for backward compatibility
+            # i.e. if type(req) == str and req.startswith("predict:")
+            # pls remove this block after all models are updated
+            # pls keep the second if block (req["task"] == "inference":)
             if type(req) == str and req.startswith("predict:"):
                 payload = json.loads(req.split(":", 1)[1])
                 prediction = model.process_request(payload)
