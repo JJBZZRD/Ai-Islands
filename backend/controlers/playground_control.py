82abbcecd17c9a14e68cf366990552dfada3e2fe
JJBZZRD <buzzard.jj@gmail.com>
2024-07-24 17:45:29 +0100

    initial playground skeleton

---
+++ backend/controlers/playground_control.py
@@ -0,0 +1,44 @@
+
+
+
+
+class PlaygroundControl:
+    def __init__(self, model_control):
+        self.model_control = model_control
+        self.playgrounds = {}
+    
+    def create_playground(self):
+        pass
+    
+    def delete_playground(self):
+        pass
+    
+    def update_playground(self):
+        pass
+    
+    def list_playgrounds(self):
+        pass
+    
+    def add_model_to_playground(self):
+        pass
+    
+    def remove_model_from_playground(self):
+        pass
+    
+    def run_playground(self):
+        pass
+    
+    def stop_playground(self):
+        pass
+    
+    def add_chain_to_playground(self):
+        pass
+    
+    def remove_chain_from_playground(self):
+        pass
+    
+    def load_playground(self):
+        pass
+    
+    def save_playground(self):
+        pass
---
+++ backend/playground/__init__.py
@@ -0,0 +1,1 @@
+from .playround import Playground
