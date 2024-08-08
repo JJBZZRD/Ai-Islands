b1a934ffee69179d0baad59ac8333d01639f0f55
JJBZZRD <buzzard.jj@gmail.com>
2024-08-08 16:09:31 +0100

    created Model class and modified get library method to use it

--- frontend/Models/Model.cs
+++ frontend/Models/Model.cs
@@ -1,25 +1,67 @@
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using frontend.Models;
 using Newtonsoft.Json.Linq;
+using System.Reflection;
 
 namespace frontend.Models
 {
     public class Model
     {
-        //Define instance variable with all possible first order keys from library.json
+        // Define instance variables with all possible first order keys from library.json
+        public string? ModelId { get; set; }
+        public string? BaseModel { get; set; }
+        public string? Dir { get; set; }
+        public bool? IsCustomised { get; set; }
+        public bool? IsOnline { get; set; }
+        public string? ModelSource { get; set; }
+        public List<string>? Tags { get; set; }
+        public List<string>? RequiredClasses { get; set; }
+        public string? ModelDesc { get; set; }
+        public string? ModelDetail { get; set; }
+        public string? ModelClass { get; set; }
+        public string? PipelineTag { get; set; }
+        public string? ModelCardUrl { get; set; }
+        public JObject? Requirements { get; set; }
+        public JObject? Config { get; set; }
+        public string? AuthToken { get; set; }
 
         public Model(JObject json)
         {
-            processJson(json);
-            
+            ProcessJson(json);
         }
 
-        private void processJson(JObject json)
+        private void ProcessJson(JObject json)
         {
-         // some loop which correctly assigns all first order keys from json to instance variables, copying the data over.
+            foreach (var pair in json)
+            {
+                string propertyName = ToPascalCase(pair.Key);
+                var property = GetType().GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                if (property != null && pair.Value != null)
+                {
+                    Type propertyType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+
+                    if (propertyType == typeof(List<string>))
+                    {
+                        property.SetValue(this, pair.Value.ToObject<List<string>>());
+                    }
+                    else if (propertyType == typeof(JObject))
+                    {
+                        property.SetValue(this, pair.Value.ToObject<JObject>());
+                    }
+                    else
+                    {
+                        property.SetValue(this, pair.Value.ToObject(propertyType));
+                    }
+                }
+            }
+        }
+
+        private static string ToPascalCase(string name)
+        {
+            string camelCase = JsonNamingPolicy.CamelCase.ConvertName(name);
+            return char.ToUpper(camelCase[0]) + camelCase.Substring(1);
         }
     }
 }
-
-Model model = new Model(json);
--- frontend/Services/LibraryService.cs
+++ frontend/Services/LibraryService.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json.Linq;
+using frontend.Models;
 
 namespace frontend.Services
 {
@@ -20,11 +23,33 @@ namespace frontend.Services
         }
 
 
-        public async Task<Dictionary<string, Dictionary<string, object>>> GetLibrary()
+        public async Task<ObservableCollection<Model>> GetLibrary()
         {
             var response = await _httpClient.GetAsync("library/get-full-library");
             response.EnsureSuccessStatusCode();
-            return (await response.Content.ReadFromJsonAsync<Dictionary<string, Dictionary<string, object>>>())!;
+            var jsonString = await response.Content.ReadAsStringAsync();
+            
+            // Parse the JSON string into a JObject
+            var libraryJson = JObject.Parse(jsonString);
+            
+            var modelCollection = new ObservableCollection<Model>();
+
+            // Iterate through each key-value pair in the JObject
+            foreach (var pair in libraryJson)
+            {
+                if (pair.Value is JObject modelData)
+                {
+                    // Create a new JObject for each model, including the key as "ModelId"
+                    var modelJson = new JObject(modelData);
+                    modelJson["ModelId"] = pair.Key;
+
+                    // Create a new Model instance and add it to the collection
+                    var model = new Model(modelJson);
+                    modelCollection.Add(model);
+                }
+            }
+
+            return modelCollection;
         }
 
         public async Task<Dictionary<string, Dictionary<string, object>>> GetModelIndex()
