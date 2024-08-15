922345b3e883ada04c22314830e3c901e12f7da1
bennylao <bennylao830@gmail.com>
2024-08-15 18:57:46 +0100

    rebase from main

---
+++ frontend/Models/ViewModels/PlaygroundViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace frontend.Models.ViewModels
+{
+    internal partial class PlaygroundViewModel : ObservableObject
+    {
+        private Entities.Playground playground;
+    }
+}
