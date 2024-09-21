c1e7ea019988f22b1c2e098114b1886b9393f04e
CostasHadjineophytou <constantinos.hadjineophytou.23@ucl.ac.uk>
2024-09-21 22:03:32 +0100

    chaptbot UI

---
+++ frontend/Converters/HalfWidthConverter.cs
@@ -0,0 +1,21 @@
+using System.Globalization;
+
+namespace frontend.Converters
+{
+    public class HalfWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                return width / 2;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
